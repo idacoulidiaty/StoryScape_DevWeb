145d14ab25708ab800d3d48f3b9ae5b7fc5a9d43
ida.coulidiaty <ida.coulidiaty@le-campus-numerique.fr>
2024-07-17 16:29:46 +0200

    new test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template
 from os import listdir
 from os.path import isfile, join
 
-from ia import test
+from ia import *
 
 
 app = Flask(__name__)
@@ -16,10 +16,17 @@ def index():
 
 @app.route("/gallery")
 def gallery():
-    images_livre = ['images/' + image for image in listdir('images') if isfile(join('images', image))]
+    images_livre = get_images_paths('static/images')
+    image_path_and_stories = []
+    for image in images_livre:
+        story = generate_story(image)
+        image_html = '../' + image
+        image_path_and_stories.append( (story, image_html) )
+
     return render_template('gallery.html', images=images_livre)
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- ia.py
+++ ia.py
@@ -3,35 +3,35 @@ import ollama
 from os import listdir
 from os.path import isfile, join
 
+def get_images_paths(folder_path):
+    images_livre = ['./images/' + image for image in listdir(folder_path) if isfile(join(folder_path, image))]
+    return images_livre
 
-
-images_livre = ['./images/' + image for image in listdir('images') if isfile(join('images', image))]
-#print (images_livre)
-
-
-#image_path = './static/images/ptit_chaperon.jpeg'
-
-descriptions = []
-
-
-for image in images_livre:
+def generate_story(path):
     res = ollama.chat(
         model='llava',
         messages=[
             {
                 'role': 'user', 
-                'content': 'Generate a title and a fun and short children story based on this image',
-                'images': [image]
+                'content': 'Generate a title and a fun 3 lines children story based on this image. ',
+                'images': [path]
             }
         ],
     )
+
+    return res['message']['content']
+
+
+
+if __name__ == "__main__":
     
-   
-    descriptions.append(res['message']['content'])
+    images_livre = get_images_paths('static/images')
 
+    for image in images_livre:
+        story = generate_story(image)
+        print(story)
+        print('\n-----------------\n')
 
-for description in descriptions:
-    print(description)
 
 
 
