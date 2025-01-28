ba25bdfdb6f850d018f9097a688e6dc2c4317b56
ida.coulidiaty <ida.coulidiaty@le-campus-numerique.fr>
2025-01-28 10:54:37 +0100

    commit sous-dossier projet

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import glob
-from ia import generate_story  # Assurez-vous que cette fonction est correctement importée
+from ia import generate_story  
 
 app = Flask(__name__)
 
@@ -11,13 +11,13 @@ def index():
 @app.route("/gallery")
 def gallery():
     # Récupérer les chemins des images
-    urls = glob.glob('static/images/*.jpeg')  # Adapter l'extension si nécessaire
+    urls = glob.glob('static/images/*.jpeg') 
     image_path_and_stories = []
 
     # Générer une histoire pour chaque image
     for url in urls:
-        story = generate_story(url)  # Appeler la fonction pour générer l'histoire
-        url_html = url.replace('static/', '')  # Adapter le chemin pour le HTML
+        story = generate_story(url)  
+        url_html = url.replace('static/', '')  
         image_path_and_stories.append((story, url_html))
 
     return render_template('gallery.html', images=image_path_and_stories)
